3a64891d75ee064630c5391299ba5ea1b96d101d
Md Monir Hossain Showrav <18103142@iubat.edu>
2020-11-07 22:17:34 +0600

    add preloader

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import axios from "axios";
+import Spinner from 'react-bootstrap/Spinner';
 export default class Home extends React.Component {
-    state = {
-      information: []
-    }
-    componentDidMount(){
-      axios.get(`https://jsonplaceholder.typicode.com/posts`)
-       // GET request using axios inside componentDidMount React hook
-        .then(response => {
-          const information = response.data;
-          this.setState({ information });
-        })
-    }
-    render() {
-      return (
-
-        <ol>
-          { this.state.information.map(info => <li> Title: {info.title} <br/> body: {info.body}</li>)}
-        </ol>
-      )
-    }
+  state = {
+    information: []
   }
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      // GET request using axios inside componentDidMount React hook
+      .then(res => {
+        const information = res.data;
+        this.setState({ information });
+      })
+  }
+  render() {
+    return (
+      this.state.information.length == 0 ? <Spinner animation="border" /> : <ol>
+        {this.state.information.map(info => <li> Title: {info.title} <br /> body: {info.body}</li>)}
+      </ol>
+
+    )
+  }
+}
