0bb890a42c4662b5cb896ff03f5129bf26749c50
Md Monir Hossain Showrav <18103142@iubat.edu>
2020-11-08 22:41:02 +0600

    :ambulance: Error Handeling

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,25 +4,34 @@ import Spinner from 'react-bootstrap/Spinner';
 export default class Home extends React.Component {
   state = {
     information: [],
-    err: []
+    err: ""
 
   }
   componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/posts`)
+    axios.get(`https://jsonplaceholder.typicode.com/post`)
       // GET request using axios inside componentDidMount React hook
       .then(res => {
         const information = res.data;
         this.setState({ information });
       }
-      ).catch((err) => {
-        alert(err);
-      })
+      )
+      .catch(err => this.setState({ err: err.message }))
   }
   render() {
+    // render element
+    let spinerRender = <Spinner animation="border" />;
+    let informationRender = this.state.information.length !== 0 && this.state.err.length === 0 && (
+      this.state.information.map(info => <ol><li> Title: {info.title} <br /> body: {info.body}</li></ol>)
+    )
+    let errorMessageRender = this.state.err.length !== 0 && (
+      <p>{this.state.err}</p>
+    )
     return (
-      this.state.information.length == 0 ? <Spinner animation="border" /> : <ol>
-        {this.state.information.map(info => <li> Title: {info.title} <br /> body: {info.body}</li>)}
-      </ol>
+      <>
+        {
+          informationRender ? informationRender : errorMessageRender ? errorMessageRender : spinerRender
+        }
+      </>
 
     )
   }
